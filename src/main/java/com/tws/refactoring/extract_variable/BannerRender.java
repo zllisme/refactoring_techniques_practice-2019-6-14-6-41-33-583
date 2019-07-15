b16f500272e90661f7009919577d3c56cd7f9a88
zllisme <1973842936@qq.com>
2019-07-15 22:02:58 +0800

    reactor: extract renderBanner final variable MAC and IE

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -1,8 +1,10 @@
 package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
+    private final static String MAC = "MAC";
+    private final static String IE = "IE";
     public String renderBanner(String platform, String browser) {
-        return platform.toUpperCase().contains("MAC") && browser.toUpperCase().contains("IE")
+        return platform.toUpperCase().contains(MAC) && browser.toUpperCase().contains(IE)
                 ? "IE on Mac?" : "banner";
     }
 }
