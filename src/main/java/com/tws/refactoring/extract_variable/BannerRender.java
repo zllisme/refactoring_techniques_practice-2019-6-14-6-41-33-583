8cb90c75c755c98360b9eb755efddaadf164d95e
zllisme <1973842936@qq.com>
2019-07-15 21:58:49 +0800

    reactor: change renderBanner judge condition logic

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,10 +2,7 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     public String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
-            return "IE on Mac?";
-        }
-        return "banner";
+        return platform.toUpperCase().contains("MAC") && browser.toUpperCase().contains("IE")
+                ? "IE on Mac?" : "banner";
     }
 }
